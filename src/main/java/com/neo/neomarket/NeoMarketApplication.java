dc8287d8faa8d54a228b8242dfc65257cd865370
jiione <pg15320@gmail.com>
2024-08-02 16:03:40 +0900

    init

---
+++ src/main/java/com/neo/neomarket/NeoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.neo.neomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoMarketApplication.class, args);
+	}
+
+}
