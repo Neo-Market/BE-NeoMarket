3e50a671226956edcdb8e965a1e182ebb4db7449
jiwon <pg15320@gmail.com>
2024-08-10 13:14:12 +0900

    feat(#8): update logback-spring.xml, 로그가 중복되는 이슈 수정

--- src/main/java/com/neo/neomarket/controller/AuctionController.java
+++ src/main/java/com/neo/neomarket/controller/AuctionController.java
@@ -1,13 +1,6 @@
 package com.neo.neomarket.controller;
 
-import com.neo.neomarket.dto.BidLogDTO;
-import com.neo.neomarket.dto.UserExchangeLogDTO;
-import com.neo.neomarket.service.impl.AuctionServiceImpl;
-import com.neo.neomarket.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,21 +8,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class AuctionController {
-    private final AuctionServiceImpl auctionService;
-    private final UserServiceImpl userService;
-
-
-    @PostMapping(value = "/log/auction")
-    public ResponseEntity<Void> recordBidLog(@RequestBody BidLogDTO bidLogDTO){
-        auctionService.recordBidLog(bidLogDTO);
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping(value = "/log/user")
-    public ResponseEntity<Void> recordBidLog(@RequestBody UserExchangeLogDTO userExchangeLogDTO){
-        userService.recordExchangeLog(userExchangeLogDTO);
-        return ResponseEntity.ok().build();
-    }
-
-
 }
