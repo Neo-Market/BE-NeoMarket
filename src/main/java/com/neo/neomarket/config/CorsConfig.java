70fa52680a00b2cd77484c309ad86173f2d91fb3
최수연 <tndus5383@gmail.com>
2024-08-14 03:59:05 +0900

    feat(#5): SecurityConfig 파일 설정

--- src/main/java/com/neo/neomarket/config/CorsConfig.java
+++ src/main/java/com/neo/neomarket/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")  // API 경로 패턴
+        registry.addMapping("/**")  // API 경로 패턴
                 .allowedOrigins("http://localhost:3000")  // React 앱의 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
