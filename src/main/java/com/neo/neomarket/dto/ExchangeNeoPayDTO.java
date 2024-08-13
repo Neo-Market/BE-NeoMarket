c863d30b8450f0ab8a5033eb6ddcfe1e322dc710
jiwon <pg15320@gmail.com>
2024-08-13 22:33:26 +0900

    feat(#41): Swagger UI 초기 셋팅 및 구현

--- src/main/java/com/neo/neomarket/dto/ExchangeNeoPayDTO.java
+++ src/main/java/com/neo/neomarket/dto/ExchangeNeoPayDTO.java
@@ -1,11 +1,15 @@
 package com.neo.neomarket.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ExchangeNeoPayDTO {
+    @Schema(description = "유저 id")
     private Long userId;
+
+    @Schema(description = "네오 페이 포인트")
     private Long point;
 }
