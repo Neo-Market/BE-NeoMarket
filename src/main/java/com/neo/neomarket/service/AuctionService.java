3c648836a3902da5a3da40096c2107f881ee1a77
na-rong <129728196+na-rong@users.noreply.github.com>
2024-08-13 12:23:15 +0900

    feat(#30) : AuctionPost CRUD 구현

--- src/main/java/com/neo/neomarket/service/AuctionService.java
+++ src/main/java/com/neo/neomarket/service/AuctionService.java
@@ -1,7 +1,16 @@
 package com.neo.neomarket.service;
 
+import com.neo.neomarket.dto.AuctionPostDTO;
 import com.neo.neomarket.dto.BidLogDTO;
+
+import java.util.List;
 
 public interface AuctionService {
     void recordBidLog(BidLogDTO bidLogDTO);
+    List<AuctionPostDTO> getAuctionPosts();
+    AuctionPostDTO getAuctionPostById(Long id);
+    AuctionPostDTO createAuctionPost(AuctionPostDTO auctionPostDTO);
+    AuctionPostDTO updateAuctionPost(Long id, AuctionPostDTO auctionPostDTO);
+    void deleteAuctionPost(Long id);
+
 }
