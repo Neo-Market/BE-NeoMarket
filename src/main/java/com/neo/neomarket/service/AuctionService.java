ff35e1e21617dc29978a63846961eaabf0a36026
jiwon <pg15320@gmail.com>
2024-08-14 17:36:16 +0900

    feat(#50): ES에서 로그 데이터를 기반으로 입찰 & 낙찰 하는 기능

--- src/main/java/com/neo/neomarket/service/AuctionService.java
+++ src/main/java/com/neo/neomarket/service/AuctionService.java
@@ -1,7 +1,10 @@
 package com.neo.neomarket.service;
 
 import com.neo.neomarket.dto.BidLogDTO;
+import com.neo.neomarket.dto.BidRequestDTO;
 
 public interface AuctionService {
     void recordBidLog(BidLogDTO bidLogDTO);
+    void bidAction(BidRequestDTO bidRequestDTO);
+    void bidSuccessAction(Long postId);
 }
