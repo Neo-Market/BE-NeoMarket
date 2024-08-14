85f1a9bcd08f63fd305aa9263c606d85ad6124f6
na-rong <129728196+na-rong@users.noreply.github.com>
2024-08-14 11:11:24 +0900

    feat(#30):properties 변경

--- src/main/java/com/neo/neomarket/config/ElasticSearchConfig.java
+++ src/main/java/com/neo/neomarket/config/ElasticSearchConfig.java
@@ -17,7 +17,7 @@ public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
 
-                .connectedTo("3.38.115.168:9200")
+                .connectedTo("elasticsearch:9200")
 
                 .build();
         return RestClients.create(clientConfiguration).rest();
