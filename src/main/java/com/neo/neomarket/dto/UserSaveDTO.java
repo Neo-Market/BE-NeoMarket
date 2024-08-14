5bd75b79e358272a4902bde13a93869d979c5f64
최수연 <tndus5383@gmail.com>
2024-08-15 00:19:48 +0900

    feat(#51): 회원가입 진행시 role 필드는 Role.USER로 기본 설정 - serEntity role 필드타입 String -> Enum으로 변경

--- src/main/java/com/neo/neomarket/dto/UserSaveDTO.java
+++ src/main/java/com/neo/neomarket/dto/UserSaveDTO.java
@@ -1,5 +1,6 @@
 package com.neo.neomarket.dto;
 
+import com.neo.neomarket.entity.mysql.Role;
 import com.neo.neomarket.entity.mysql.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,6 +22,10 @@ public class UserSaveDTO {
 
     private String bankName;
 
+    private String role;
+
+    private Long point;
+
     public UserEntity toEntity(OAuth2User principal) {
 
         return UserEntity.builder()
@@ -31,6 +36,8 @@ public class UserSaveDTO {
                 .address(this.address)
                 .accountNumber(this.accountNumber)
                 .bankName(this.bankName)
+                .role(Role.USER)
+                .point(0L)
                 .build();
     }
 }
