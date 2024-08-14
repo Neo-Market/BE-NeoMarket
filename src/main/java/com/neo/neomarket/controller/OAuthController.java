2794e7ea1c4782024a7c3797944893389726153f
최수연 <tndus5383@gmail.com>
2024-08-15 00:29:11 +0900

    feat(#51): OAuth 프로퍼티 설정 추가

--- src/main/java/com/neo/neomarket/controller/OAuthController.java
+++ src/main/java/com/neo/neomarket/controller/OAuthController.java
@@ -1,16 +1,12 @@
 package com.neo.neomarket.controller;
 
 
-import com.neo.neomarket.dto.AuthResultDTO;
-import jakarta.servlet.http.HttpSession;
-import java.util.Map;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -29,28 +25,29 @@ public class OAuthController {
     private String redirectUri;
 
     @GetMapping("/api/login")
-    public ResponseEntity<Map<String, String>> getGoogleAuthUrl() {
+    public void redirectToGoogleAuth(HttpServletResponse response) throws IOException {
         String authUrl = UriComponentsBuilder.fromHttpUrl("https://accounts.google.com/o/oauth2/v2/auth")
                 .queryParam("client_id", googleClientId)
                 .queryParam("redirect_uri", redirectUri)
                 .queryParam("response_type", "code")
-                .queryParam("scope", "email profile")
+                .queryParam("scope", "openid profile email")
                 .build().toUriString();
 
+        // 로그로 OAuth2 URL을 출력
         logger.info("Generated Google OAuth2 URL: " + authUrl);
 
-        return ResponseEntity.ok(Map.of("authorizationUrl", authUrl));
+        // 클라이언트를 구글 인증 페이지로 리디렉션
+        response.sendRedirect(authUrl);
     }
 
-    @GetMapping("/login/oauth2/code/google")
-    public ResponseEntity<AuthResultDTO> googleCallback(@AuthenticationPrincipal OAuth2User principal) {
-        if (principal != null) {
-            return ResponseEntity.ok(new AuthResultDTO(LOGIN_SUCCESS_MESSAGE));
-        } else {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new AuthResultDTO(LOGIN_FAILURE_MESSAGE));
-        }
-    }
+//    @GetMapping("/login/oauth2/code/google")
+//    public void googleCallback(@AuthenticationPrincipal OAuth2User principal, HttpServletResponse response)
+//            throws IOException {
+//        if (principal != null) {
+//            response.sendRedirect("/register");
+//        } else {
+//            response.sendRedirect("/login?error");
+//        }
+//    }
 
 }
