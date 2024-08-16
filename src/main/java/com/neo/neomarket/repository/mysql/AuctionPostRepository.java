79dc534f286b62b8bcfe7270399b07ef76645e3a
jiwon <pg15320@gmail.com>
2024-08-16 12:31:06 +0900

    hotfix-homeUpdate

--- src/main/java/com/neo/neomarket/repository/mysql/AuctionPostRepository.java
+++ src/main/java/com/neo/neomarket/repository/mysql/AuctionPostRepository.java
@@ -3,5 +3,8 @@ package com.neo.neomarket.repository.mysql;
 import com.neo.neomarket.entity.mysql.AuctionPostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuctionPostRepository extends JpaRepository<AuctionPostEntity, Long> {
+    List<AuctionPostEntity> findAllByOrderByCreatedDateDesc();
 }
