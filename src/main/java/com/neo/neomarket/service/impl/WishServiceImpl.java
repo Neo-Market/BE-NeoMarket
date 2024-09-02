ef6808107c19aa067b8202e4c0f1bd9ecc29b09d
최수연 <tndus5383@gmail.com>
2024-09-02 17:27:09 +0900

    feat: 위시리스트 추가/삭제 API 구현

--- src/main/java/com/neo/neomarket/service/impl/WishServiceImpl.java
+++ src/main/java/com/neo/neomarket/service/impl/WishServiceImpl.java
@@ -1,6 +1,6 @@
 package com.neo.neomarket.service.impl;
 
-import com.neo.neomarket.dto.WishDTO;
+import com.neo.neomarket.dto.user.WishDTO;
 import com.neo.neomarket.entity.mysql.WishEntity;
 import com.neo.neomarket.entity.mysql.UserEntity;
 import com.neo.neomarket.entity.mysql.AuctionPostEntity;
@@ -14,57 +14,57 @@ import com.neo.neomarket.repository.mysql.WishRepository;
 import com.neo.neomarket.service.WishService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class WishServiceImpl implements WishService {
 
-        private final WishRepository wishRepository;
+    private final WishRepository wishRepository;
+    private final UserRepository userRepository;
+    private final AuctionPostRepository auctionPostRepository;
+    private final UsedPostRepository usedPostRepository;
 
-        private final UserRepository userRepository;
+    @Override
+    public Long addWish(WishDTO wishDTO) {
+        UserEntity user = userRepository.findById(wishDTO.getUserId())
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_USER));
 
-        private final AuctionPostRepository auctionPostRepository;
+        WishEntity wishEntity;
 
-        private final UsedPostRepository usedPostRepository;
+        if (wishDTO.getPostType().equals("경매")) {
+            AuctionPostEntity auctionPost = auctionPostRepository.findById(wishDTO.getPostId())
+                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_POST));
 
-        @Override
-        public void addToWish(WishDTO wishDTO) {
-            UserEntity user = userRepository.findById(wishDTO.getUserId())
-                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_USER));
+            wishEntity = WishEntity.builder()
+                    .user(user)
+                    .auctionPost(auctionPost)
+                    .build();
 
-            WishEntity wishEntity = new WishEntity();
-            wishEntity.setUser(user);
+        } else if (wishDTO.getPostType().equals("중고")) {
+            UsedPostEntity usedPost = usedPostRepository.findById(wishDTO.getPostId())
+                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_POST));
 
-            if (wishDTO.getPostType() == 0) { // AuctionPost
-                AuctionPostEntity auctionPost = auctionPostRepository.findById(wishDTO.getPostId())
-                        .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_POST));
-                wishEntity.setAuctionPost(auctionPost);
-            } else if (wishDTO.getPostType() == 1) { // UsedPost
-                UsedPostEntity usedPost = usedPostRepository.findById(wishDTO.getPostId())
-                        .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_POST));
-                wishEntity.setUsedPost(usedPost);
-            } else {
-                throw new CustomException(ErrorCode.NOT_EXIST_POSTTYPE);
-            }
+            wishEntity = WishEntity.builder()
+                    .user(user)
+                    .usedPost(usedPost)
+                    .build();
 
-            wishRepository.save(wishEntity);
-        }
-
-        @Override
-        public void removeFromWish(WishDTO wishDTO) {
-        if (wishDTO.getPostType() == 0) { // AuctionPost
-            wishRepository.findByUserIdAndAuctionPostId(wishDTO.getUserId(), wishDTO.getPostId())
-                    .ifPresentOrElse(wishRepository::delete, () -> {
-                        throw new CustomException(ErrorCode.NOT_EXIST_WISHLIST);
-                    });
-        } else if (wishDTO.getPostType() == 1) { // UsedPost
-            wishRepository.findByUserIdAndUsedPostId(wishDTO.getUserId(), wishDTO.getPostId())
-                    .ifPresentOrElse(wishRepository::delete, () -> {
-                        throw new CustomException(ErrorCode.NOT_EXIST_WISHLIST);
-                    });
         } else {
             throw new CustomException(ErrorCode.NOT_EXIST_POSTTYPE);
         }
+
+        WishEntity saved = wishRepository.save(wishEntity);
+        return saved.getId();
+    }
+
+    @Override
+    public void removeWish(Long id) {
+        WishEntity wishEntity = wishRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_WISHLIST));
+
+        wishRepository.delete(wishEntity);
     }
 
 }
