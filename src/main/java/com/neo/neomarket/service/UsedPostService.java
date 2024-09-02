2fe132835622766d79766b44e913838d15650233
최수연 <tndus5383@gmail.com>
2024-09-02 17:29:38 +0900

    refactor: 중고 게시글 로직 리팩토링

--- src/main/java/com/neo/neomarket/service/UsedPostService.java
+++ src/main/java/com/neo/neomarket/service/UsedPostService.java
@@ -1,16 +1,21 @@
 package com.neo.neomarket.service;
 
 import com.neo.neomarket.dto.usedPost.UsedPostCreateDTO;
-import com.neo.neomarket.dto.usedPost.UsedPostDTO;
-import com.neo.neomarket.dto.usedPost.UsedPostIdDTO;
+import com.neo.neomarket.dto.usedPost.UsedPostShowDTO;
+import com.neo.neomarket.dto.usedPost.UsedPostDetailDTO;
 import com.neo.neomarket.dto.usedPost.UsedPostUpdateDTO;
 
 import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
-public interface UsedPostService    {
-    List<UsedPostDTO> getUsedPosts();
-    UsedPostIdDTO findPostById(Long id);
-    UsedPostCreateDTO createPost(UsedPostCreateDTO usedPostCreateDTO);
-    void updatePost(Long id, UsedPostUpdateDTO usedPostUpdateDTO);
-    void deletePost(Long id);
+public interface UsedPostService {
+    List<UsedPostShowDTO> getUsedPosts();
+
+    UsedPostDetailDTO findUsedPostById(Long id);
+
+    Long createUsedPost(UsedPostCreateDTO usedPostCreateDTO, List<MultipartFile> pictures);
+
+    void updateUsedPost(Long id, UsedPostUpdateDTO usedPostUpdateDTO);
+
+    void deleteUsedPost(Long id);
 }
--- src/main/java/com/neo/neomarket/service/impl/UsedPostServiceImpl.java
+++ src/main/java/com/neo/neomarket/service/impl/UsedPostServiceImpl.java
@@ -1,9 +1,10 @@
 package com.neo.neomarket.service.impl;
 
 import com.neo.neomarket.dto.usedPost.UsedPostCreateDTO;
-import com.neo.neomarket.dto.usedPost.UsedPostDTO;
-import com.neo.neomarket.dto.usedPost.UsedPostIdDTO;
+import com.neo.neomarket.dto.usedPost.UsedPostShowDTO;
+import com.neo.neomarket.dto.usedPost.UsedPostDetailDTO;
 import com.neo.neomarket.dto.usedPost.UsedPostUpdateDTO;
+import com.neo.neomarket.entity.mysql.PictureEntity;
 import com.neo.neomarket.entity.mysql.UsedPostEntity;
 import com.neo.neomarket.entity.mysql.UserEntity;
 import com.neo.neomarket.exception.CustomException;
@@ -18,6 +19,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Service
@@ -29,103 +32,68 @@ public class UsedPostServiceImpl implements UsedPostService {
     private final PictureRepository pictureRepository;
 
     @Override
-    public List<UsedPostDTO> getUsedPosts() {
-        List<UsedPostEntity> usedPostEntities = usedPostRepository.findAll();
-
-        if (usedPostEntities.isEmpty()) {
-            return List.of();
-        }
-
-        return usedPostEntities.stream()
-                .map(entity -> UsedPostDTO.builder()
-                        .id(entity.getId())
-                        .title(entity.getTitle())
-                        .price(entity.getPrice())
-                        .createTime(entity.getCreatedDate())
-                        .category(entity.getCategory())
-                        .nickname(entity.getUser().getNickname())
-                        .build())
-                .toList();
+    @Transactional(readOnly = true)
+    public List<UsedPostShowDTO> getUsedPosts() {
+        List<UsedPostShowDTO> usedPostShowDTOList = usedPostRepository.findAllByOrderByLastModifiedDateDesc().stream()
+                .map(entity -> UsedPostShowDTO.builder().id(entity.getId()).title(entity.getTitle())
+                        .price(entity.getPrice()).picture(entity.getPictures().get(0).getUrl())
+                        .nickname(entity.getUser().getNickname()).createTime(entity.getCreatedDate())
+                        .category(entity.getCategory()).build()).toList();
+        return usedPostShowDTOList;
     }
 
     @Override
-    public UsedPostIdDTO findPostById(Long id) {
-        // 게시글을 ID로 조회합니다.
-        UsedPostEntity findPost = usedPostRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_POST));
-        String nickname = findPost.getUser().getNickname();
+    @Transactional(readOnly = true)
+    public UsedPostDetailDTO findUsedPostById(Long id) {
+        UsedPostEntity findPost = usedPostRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_POST));
+
         List<String> pictureUrls = new ArrayList<>();
         findPost.getPictures().forEach(pictureEntity -> pictureUrls.add(pictureEntity.getUrl()));
 
-        UsedPostIdDTO usedPostIdDTO = UsedPostIdDTO.builder()
-                .title(findPost.getTitle())
-                .content(findPost.getContent())
-                .price(findPost.getPrice())
-                .nickname(nickname)
-                .createTime(findPost.getCreatedDate())
-                .views(findPost.getViews())
-                .category(findPost.getCategory())
-                .build();
-
-        return usedPostIdDTO;
+        return UsedPostDetailDTO.builder().title(findPost.getTitle()).content(findPost.getContent())
+                .price(findPost.getPrice()).nickname(findPost.getUser().getNickname()).views(findPost.getViews())
+                .createTime(findPost.getCreatedDate()).category(findPost.getCategory()).pictures(pictureUrls).build();
     }
 
     @Override
-    public UsedPostCreateDTO createPost(UsedPostCreateDTO usedPostCreateDTO) {
-        UserEntity user = userRepository.findById(usedPostCreateDTO.getUserId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_USER));
+    @Transactional
+    public Long createUsedPost(UsedPostCreateDTO usedPostCreateDTO, List<MultipartFile> pictures) {
+        UserEntity user = userRepository.findById(usedPostCreateDTO.getUserId())
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_USER));
 
-        // DTO를 Entity로 변환
-        UsedPostEntity usedPostEntity = UsedPostEntity.builder()
-                .title(usedPostCreateDTO.getTitle())
-                .category(usedPostCreateDTO.getCategory())
-                .content(usedPostCreateDTO.getContent())
-                .price(usedPostCreateDTO.getPrice())
-                .user(user)
-                .build();
+        UsedPostEntity usedPostEntity = UsedPostEntity.builder().title(usedPostCreateDTO.getTitle())
+                .category(usedPostCreateDTO.getCategory()).content(usedPostCreateDTO.getContent())
+                .price(usedPostCreateDTO.getPrice()).user(user).build();
 
-        // Entity를 데이터베이스에 저장
-        UsedPostEntity create = usedPostRepository.save(usedPostEntity);
+        usedPostCreateDTO.getPictureUrls().forEach(pictureUrl -> {
+            PictureEntity pictureEntity = PictureEntity.builder().url(pictureUrl).build();
+            usedPostEntity.getPictures().add(pictureEntity);
+        });
 
-        // 저장된 Entity를 다시 DTO로 변환하여 반환
-        UsedPostCreateDTO CreateDTO = UsedPostCreateDTO.builder()
-                .title(create.getTitle())
-                .category(create.getCategory())
-                .content(create.getContent())
-                .price(create.getPrice())
-                .userId(create.getUser().getId())
-                .build();
-
-        return CreateDTO;
+        UsedPostEntity created = usedPostRepository.save(usedPostEntity);
+        return created.getId();
     }
 
     @Override
-    public void updatePost(Long id, UsedPostUpdateDTO usedPostUpdateDTO) {
-        // 게시글을 ID로 조회합니다.
-        UsedPostEntity upost = usedPostRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_POST));
+    @Transactional
+    public void updateUsedPost(Long id, UsedPostUpdateDTO usedPostUpdateDTO) {
+        UsedPostEntity usedPostEntity = usedPostRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_POST));
 
-        // 게시글이 존재하는 경우, 업데이트할 필드를 설정합니다.
-        upost.setTitle(usedPostUpdateDTO.getTitle());
-        upost.setCategory(usedPostUpdateDTO.getCategory());
-        upost.setContent(usedPostUpdateDTO.getContent());
-        upost.setPrice(usedPostUpdateDTO.getPrice());
+        UserEntity user = userRepository.findByNickname(usedPostUpdateDTO.getNickname())
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_USER));
 
-        // 업데이트된 게시글을 저장합니다.
-        UsedPostEntity updatedPost = usedPostRepository.save(upost);
-
-        // Entity를 DTO로 변환하여 반환합니다.
-        UsedPostUpdateDTO UpdatDTO = UsedPostUpdateDTO.builder()
-                .title(updatedPost.getTitle())
-                .category(updatedPost.getCategory())
-                .content(updatedPost.getContent())
-                .price(updatedPost.getPrice())
-                .build();
-
+        UsedPostEntity entity = usedPostUpdateDTO.toEntity(user, id);
+        usedPostRepository.save(entity);
     }
 
     @Override
-    public void deletePost(Long id) {
-        if (!usedPostRepository.existsById(id)) {
-            throw new RuntimeException("게시물이 없습니다.");
-        }
+    @Transactional
+    public void deleteUsedPost(Long id) {
+        UsedPostEntity usedPostEntity = usedPostRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_POST));
+
         usedPostRepository.deleteById(id);
     }
 }
