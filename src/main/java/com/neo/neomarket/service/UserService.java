e87f9d003cfede07dfeb78bd0a1a6dabe07afbca
최수연 <tndus5383@gmail.com>
2024-09-08 09:59:00 +0900

    fix(#79): OAuth2User 변수 principal -> oAuth2user로 수정

--- src/main/java/com/neo/neomarket/service/UserService.java
+++ src/main/java/com/neo/neomarket/service/UserService.java
@@ -1,21 +1,27 @@
 
 package com.neo.neomarket.service;
 
-import com.neo.neomarket.dto.home.PostShowDTO;
+import com.neo.neomarket.dto.wish.PostShowDTO;
 import com.neo.neomarket.dto.user.ExchangeNeoPayDTO;
 import com.neo.neomarket.dto.user.UserExchangeLogDTO;
-import com.neo.neomarket.dto.user.UserSaveDTO;
 import com.neo.neomarket.dto.user.UserInfoDTO;
-import com.neo.neomarket.dto.user.WishDTO;
+import com.neo.neomarket.dto.user.UserSaveDTO;
 import java.util.List;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 public interface UserService {
-    Long createUser(OAuth2User principal, UserSaveDTO userSaveDTO);
-    UserInfoDTO getCurrentUserInfo(OAuth2User principal);
-    UserInfoDTO getUserInfo(OAuth2User principal, Long id);
+    Long createUser(OAuth2User oAuth2User, UserSaveDTO userSaveDTO);
+
+    UserInfoDTO getUserInfo(OAuth2User oAuth2User);
+
+    UserInfoDTO getUserInfoByEmail(String email);
+
     void recordExchangeLog(UserExchangeLogDTO userExchangeLogDTO);
+
     List<PostShowDTO> findAllWish(Long id);
+
     void chargeNeoPay(ExchangeNeoPayDTO exchangeNeoPayDTO);
+
     void exchangeNeoPay(ExchangeNeoPayDTO exchangeNeoPayDTO);
+
 }
